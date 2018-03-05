dda7bd57d43a48b3afcdcfeab5643260ce7886f4
Rien <rien@dailydialogues.nl>
2018-03-05 13:43:02 +0100

    Add the tests for the white pawn and other columns

--- chess/pieces/pawn.py
+++ chess/pieces/pawn.py
@@ -6,11 +6,15 @@ class Pawn(Piece):
     def is_valid_move(self, origin, destination):
         d_x, d_y = self._distance_between_coordinates(origin, destination)
 
-        if origin is 'b7':
-            return 1 <= d_y <= 2 and abs(d_x) == 0
+        if self.color is 'black':
+            if int(origin[1]) == 7:
+                return 1 <= d_y <= 2 and abs(d_x) == 0
 
-        if self.color == 'black':
             return d_y == 1 and abs(d_x) == 0
+
+        if int(origin[1]) == 2:
+            return -1 >= d_y >= -2 and abs(d_x) == 0
+
         return d_y == -1 and abs(d_x) == 0
 
     def __str__(self):
